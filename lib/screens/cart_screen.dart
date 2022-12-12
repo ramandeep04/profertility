69a9a9d6575806728803978084d037b77bb5f940
Barindersingh04 <barindersingh04@gmail.com>
2022-12-12 13:02:21 +0530

    cart screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:profertility/screens/widgets/my_appbar.dart';
 import 'package:profertility/screens/widgets/primary_button.dart';
 
 class CartScreen extends StatelessWidget {
@@ -7,107 +8,20 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: const MyAppBar(title: "Your Cart"),
       body: SafeArea(
         child: ListView(
           children: [
             Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 24.0, vertical: 24),
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          Image.asset("assets/images/arrow-left.png"),
-                          const SizedBox(width: 12),
-                          const Text(
-                            "Your Cart",
-                            style: TextStyle(
-                                fontSize: 18,
-                                color: Color(0xff4d1a53),
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      )
-                    ],
+                  const CartItem(),
+                  const SizedBox(
+                    height: 8,
                   ),
-                  const SizedBox(height: 25),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 22),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: const Color(0xfff7f8fa),
-                    ),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Image.asset(
-                              "assets/images/amlodipine.png",
-                            ),
-                            Column(
-                              children: [
-                                const Text(
-                                  "Amlodipine",
-                                  style: TextStyle(
-                                      color: Color(0xff1d1d1d),
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                const SizedBox(height: 6),
-                                const Text(
-                                  "Packing of 20 tablets",
-                                  style: TextStyle(
-                                      color: Color(0xff666666), fontSize: 12),
-                                ),
-                                const SizedBox(height: 12),
-                                Row(
-                                  children: const [
-                                    Text(
-                                      "\$66",
-                                      style: TextStyle(
-                                          color: Color(0xff00afa4),
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    OutlinedButton(
-                                      onPressed: () {},
-                                      style: OutlinedButton.styleFrom(
-                                        fixedSize: const Size(24, 24),
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(100),
-                                        ),
-                                      ),
-                                      child: const Icon(Icons.minimize),
-                                    ),
-                                    const Text('2'),
-                                    ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                        elevation: 0,
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(100),
-                                        ),
-                                      ),
-                                      onPressed: () {},
-                                      child: const Icon(Icons.add),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
+                  const CartItem(),
                   const SizedBox(
                     height: 20,
                   ),
@@ -140,65 +54,7 @@ class CartScreen extends StatelessWidget {
                     ),
                   ),
                   const SizedBox(height: 16),
-                  Center(
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 22,
-                        vertical: 22,
-                      ),
-                      decoration: const BoxDecoration(
-                        borderRadius:
-                            BorderRadius.only(topRight: Radius.circular(40.0)),
-                        color: Color(0xffebfafb),
-                      ),
-                      child: Wrap(
-                        runSpacing: 16.0,
-                        children: [
-                          const Text(
-                            "Payment Summary",
-                            style: TextStyle(
-                                color: Color(0xff00afa4),
-                                fontWeight: FontWeight.bold),
-                          ),
-                          const SummaryWidget(
-                            leading: "Order Total",
-                            trailing: "\$123.35",
-                          ),
-                          const SummaryWidget(
-                            leading: "Item Discount",
-                            trailing: "- \$28.00",
-                          ),
-                          const SummaryWidget(
-                            leading: "Coupon Discount",
-                            trailing: "- \$16.80",
-                          ),
-                          const SummaryWidget(
-                            leading: "Shipping",
-                            trailing: "Free",
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: const [
-                              Text(
-                                "Total",
-                                style: TextStyle(
-                                    color: Color(0xff1d1d1d),
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                "\$115.00",
-                                style: TextStyle(
-                                    color: Color(0xff00afa4),
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                  const PaymentSummary(),
                   const SizedBox(height: 22),
                   Padding(
                     padding: const EdgeInsets.only(bottom: 24),
@@ -212,6 +68,224 @@ class CartScreen extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class PaymentSummary extends StatelessWidget {
+  const PaymentSummary({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 22,
+        vertical: 22,
+      ),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(topRight: Radius.circular(40.0)),
+        color: Color(0xffebfafb),
+      ),
+      child: Wrap(
+        runSpacing: 16.0,
+        children: [
+          const Text(
+            "Payment Summary",
+            style: TextStyle(
+                color: Color(0xff00afa4), fontWeight: FontWeight.bold),
+          ),
+          const SummaryWidget(
+            leading: "Order Total",
+            trailing: "\$123.35",
+          ),
+          const SummaryWidget(
+            leading: "Item Discount",
+            trailing: "- \$28.00",
+          ),
+          const SummaryWidget(
+            leading: "Coupon Discount",
+            trailing: "- \$16.80",
+          ),
+          const SummaryWidget(
+            leading: "Shipping",
+            trailing: "Free",
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                "Total",
+                style: TextStyle(
+                    color: Color(0xff1d1d1d),
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "\$115.00",
+                style: TextStyle(
+                    color: Color(0xff00afa4),
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CouponWidget extends StatelessWidget {
+  const CouponWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class CartItem extends StatefulWidget {
+  final Size? buttonSize;
+  const CartItem({
+    Key? key,
+    this.buttonSize,
+  }) : super(key: key);
+
+  @override
+  State<CartItem> createState() => _CartItemState();
+}
+
+class _CartItemState extends State<CartItem> {
+  int amount = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      constraints: BoxConstraints.tight(
+        Size(size.width, 120),
+      ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 8.0,
+        vertical: 8.0,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: const Color(0xfff7f8fa),
+      ),
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 1,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12.0)),
+              child: Image.asset(
+                "assets/images/amlodipine.png",
+              ),
+            ),
+          ),
+          const SizedBox(width: 12.0),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Text(
+                  "Amlodipine",
+                  style: TextStyle(
+                    color: Color(0xff1d1d1d),
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Text(
+                  "Packing of 20 tablets",
+                  style: TextStyle(
+                    color: Color(0xff666666),
+                    fontSize: 12,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "\$66",
+                      style: TextStyle(
+                        color: Color(0xff00afa4),
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              amount -= 1;
+                            });
+                          },
+                          child: Container(
+                            alignment: Alignment.center,
+                            constraints: BoxConstraints.tight(
+                              widget.buttonSize ?? const Size.square(35),
+                            ),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: const Color(0xffE0E0E0),
+                              ),
+                            ),
+                            child: const Icon(
+                              Icons.remove,
+                              color: Color(0xffB1B1B3),
+                              size: 20,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 10.0),
+                        Text(
+                          "$amount",
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                        const SizedBox(width: 10.0),
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              amount += 1;
+                            });
+                          },
+                          child: Container(
+                            alignment: Alignment.center,
+                            constraints: BoxConstraints.tight(
+                             widget.buttonSize ?? const Size.square(35),
+                            ),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            child: const Icon(
+                              Icons.add,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
